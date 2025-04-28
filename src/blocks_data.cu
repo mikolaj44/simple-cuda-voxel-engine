be50d95cac31e3e6b5898f080f755ce9fbd73768
mikolaj44 <mikolajjuszczak17@gmail.com>
2025-04-28 14:01:40 +0200

    Trying to fix a bug in Phong lighting

--- src/blocks_data.cu
+++ src/blocks_data.cu
@@ -19,6 +19,9 @@ __device__ void hueToRGB(float hue, int& r, int& g, int&b){
 __device__ void getPhongIllumination(Vector3 pos, Vector3 cameraPos, Vector3 normal, Material material, PointLight light, int& r, int& g, int&b){
     material.color = Vector3(r, g, b);
 
+    //if(pos.x > 99)
+    //printf("%f %f %f\n", pos.x, pos.y, pos.z);
+
     r = 0;
     g = 0;
     b = 0;
@@ -37,7 +40,9 @@ __device__ void getPhongIllumination(Vector3 pos, Vector3 cameraPos, Vector3 nor
 	Vector3 diffuseVector = Vector3(material.diffuse, material.diffuse, material.diffuse);
 	Vector3 specularVector = Vector3(material.specular, material.specular, material.specular);
 
-	Vector3 lighting = (add(Vector3(0, 0, 0), vmul(light.color, add(mul(diffuseVector, dot(normal, Ln)), mul(specularVector, pow(dot(h, dh), material.specularExponent))))));
+	//Vector3 lighting = vmul(light.color, mul(diffuseVector, dot(normal, Ln)));
+
+    Vector3 lighting = vmul(light.color, add(mul(diffuseVector, dot(normal, Ln)), mul(specularVector, pow(dot(h, dh), material.specularExponent))));
 
 	if (lighting.x > 255)
 		lighting.x = 255;
@@ -47,11 +52,7 @@ __device__ void getPhongIllumination(Vector3 pos, Vector3 cameraPos, Vector3 nor
 		lighting.z = 255;
 
 	lighting = div(lighting, 255.0);
-
-    // if(!isnan(lighting.x))
-    // printf("%f %f %f\n", lighting.x, lighting.y, lighting.z);
-
-	lighting = vmul(lighting, Vector3(50,50,50));
+	lighting = vmul(lighting, material.color);
 
     r = (int)lighting.x;
     g = (int)lighting.y;
@@ -63,23 +64,18 @@ __device__ void getPhongIllumination(Vector3 pos, Vector3 cameraPos, Vector3 nor
 }
 
 __device__ void setPixelById(int sX, int sY, int blockX, int blockY, int blockZ, float x, float y, float z, unsigned char blockId, uchar4* pixels, Vector3 cameraPos, Material material, PointLight light, bool textureRenderingEnabled) {
-    if(!textureRenderingEnabled){
-        int r, g, b;
-        hueToRGB(float(blockId) * 2.8125 * 2.0 / 360.0, r, g, b);
-
-        //getPhongIllumination(Vector3(x, y, z), cameraPos, normal, material, light, r, g, b);
-
-        setPixel(pixels, sX, sY, r, g, b, 255);
+    if (textureRenderingEnabled && blockId >= blocksAmount) {
         return;
     }
 
-    if (blockId >= blocksAmount) {
-        return;
+    int imgWidth, imgHeight, imgChannels;
+
+    if(textureRenderingEnabled){
+        imgWidth = blockVariants[blockId]->texture->width;
+        imgHeight = blockVariants[blockId]->texture->height;
+        imgChannels = blockVariants[blockId]->texture->channels;
     }
 
-    int imgWidth = blockVariants[blockId]->texture->width;
-    int imgHeight = blockVariants[blockId]->texture->height;
-    int imgChannels = blockVariants[blockId]->texture->channels;
     int imgX = 0, imgY = 0;
 
     int r, g, b;
@@ -88,67 +84,83 @@ __device__ void setPixelById(int sX, int sY, int blockX, int blockY, int blockZ,
     // check which side of the block we are on
 
     if (equals(y, (float)blockY, epsilon)) { // top
-        imgX = (int)(absv(x - (int)x) * imgWidth);
-        imgY = (int)(absv(z - (int)z) * imgHeight);
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(x - (int)x) * imgWidth);
+            imgY = (int)(absv(z - (int)z) * imgHeight);
 
-        r = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels + 2];
-
-        normal = Vector3(0, 1, 0);
-    }
-    else if (equals(y, (float)blockY + 1.0, epsilon)) { // bottom
-        imgX = (int)(absv(x - (int)x) * imgWidth);
-        imgY = (int)(absv(z - (int)z) * imgHeight);
-
-        r = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+            r = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->topImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
 
         normal = Vector3(0, -1, 0);
     }
-    else if (equals(x, (float)blockX, epsilon)) { // left
-        imgX = (int)(absv(z - (int)z) * imgWidth);
-        imgY = (int)(absv(y - (int)y) * imgHeight);
+    else if (equals(y, (float)blockY + 1.0, epsilon)) { // bottom
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(x - (int)x) * imgWidth);
+            imgY = (int)(absv(z - (int)z) * imgHeight);
 
-        r = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels + 2];
-
-        normal = Vector3(1, 0, 0);
-    }
-    else if (equals(x, (float)blockX + 1.0, epsilon)) { // right
-        imgX = (int)(absv(z - (int)z) * imgWidth);
-        imgY = (int)(absv(y - (int)y) * imgHeight);
-
-        r = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels + 2];
-
-        normal = Vector3(-1, 0, 0);
-    }
-    else if (equals(z, (float)blockZ, epsilon)) { // front
-        imgX = (int)(absv(x - (int)x) * imgWidth);
-        imgY = (int)(absv(y - (int)y) * imgHeight);
-
-        r = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+            r = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->bottomImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
 
         normal = Vector3(0, 1, 0);
     }
-    else if (equals(z, (float)blockZ + 1.0, epsilon)) { // back
-        imgX = (int)(absv(x - (int)x) * imgWidth);
-        imgY = (int)(absv(y - (int)y) * imgHeight);
+    else if (equals(x, (float)blockX, epsilon)) { // left
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(z - (int)z) * imgWidth);
+            imgY = (int)(absv(y - (int)y) * imgHeight);
 
-        r = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels];
-        g = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels + 1];
-        b = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+            r = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->leftImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
+
+        normal = Vector3(-1, 0, 0);
+    }
+    else if (equals(x, (float)blockX + 1.0, epsilon)) { // right
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(z - (int)z) * imgWidth);
+            imgY = (int)(absv(y - (int)y) * imgHeight);
+
+            r = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->rightImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
+
+        normal = Vector3(1, 0, 0);
+    }
+    else if (equals(z, (float)blockZ, epsilon)) { // front
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(x - (int)x) * imgWidth);
+            imgY = (int)(absv(y - (int)y) * imgHeight);
+
+            r = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->frontImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
 
         normal = Vector3(0, 0, -1);
     }
+    else if (equals(z, (float)blockZ + 1.0, epsilon)) { // back
+        if(textureRenderingEnabled){
+            imgX = (int)(absv(x - (int)x) * imgWidth);
+            imgY = (int)(absv(y - (int)y) * imgHeight);
+
+            r = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels];
+            g = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels + 1];
+            b = blockVariants[blockId]->texture->backImage[(imgY * imgWidth + imgX) * imgChannels + 2];
+        }
+
+        normal = Vector3(0, 0, 1);
+    }
     else{
         return;
+    }
+    
+    if(!textureRenderingEnabled) {
+        hueToRGB(float(blockId) * 2.8125 * 2.0 / 360.0, r, g, b);
     }
 
     getPhongIllumination(Vector3(x, y, z), cameraPos, normal, material, light, r, g, b);
